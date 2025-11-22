53b706ede35efd305e083d37c302aeea7296b7fd
Pham Buasawan <158278417+maxwellisnothere@users.noreply.github.com>
2025-11-23 00:39:13 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // 👇 เพิ่มก้อนนี้เข้าไปครับ 👇
+  server: {
+    host: true, // อนุญาตให้เครื่องอื่น (ngrok) เข้าถึงได้
+    allowedHosts: ['neriah-indirect-manie.ngrok-free.dev'], // อนุญาตโดเมน ngrok ของนาย (สำคัญ!)
+  }
 })
